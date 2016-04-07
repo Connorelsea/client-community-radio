4bf39ced550f2048e797e48187827b09d8519d6e
Connorelsea <connorelsea@gmail.com>
2016-04-07 12:24:57 -0500

    Removed /lib and fixed local database connection

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mysql from "mysql"
 
 const host = "localhost"
-const user = "mysql"
+const user = "root"
 const password = ""
 const database = "radio"
 
@@ -9,12 +9,13 @@ const connection = mysql.createConnection({
     host, user, password, database
 })
 
+
 const connect = function() {
 
     connection.connect(function(err) {
 
         if (err) {
-            console.log("MySQL: Connection error")
+            console.log(`MySQL: Connection error: ${err}`)
         } else {
             console.log("MySQL: Connected successfully")
         }
